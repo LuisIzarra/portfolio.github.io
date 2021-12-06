0ae623edc21195cb186a4de17ad84cce04c28324
LuisIzarra <lrodriguezizarra@gmail.com>
2021-12-06 12:37:39 -0500

    routing home address

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Switch>
-          <Route path="/" exact component={Home} />
+          <Route path="/portfolio.github.io" exact component={Home} />
           <Route path="/portfolio.github.io" component={Home} />
           <Route path="/project" component={Projects} />
           <Route path="/about" component={About} />
