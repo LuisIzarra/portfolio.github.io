1b02f9300fcc7392512d09d50d839ec4370159df
LuisIzarra <lrodriguezizarra@gmail.com>
2022-01-28 14:06:43 -0500

    palette color adjust github's activity

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,11 +6,11 @@ function Github() {
   const colourTheme = {
     background: "transparent",
     text: "#ffffff",
-    grade4: "#8400b8",
-    grade3: "#b22ff4",
-    grade2: "#b265f6",
-    grade1: "#c084f5",
-    grade0: "#ecd9fc",
+    grade4: "#0469DC",
+    grade3: "#3082DF",
+    grade2: "#4E96E8",
+    grade1: "#6EB0FB",
+    grade0: "#C7E1FF",
   };
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
